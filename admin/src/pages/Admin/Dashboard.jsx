663143ca63cba9a415da4f616349ddd64670b49c
SWKHansala <164784128+SWKHansala@users.noreply.github.com>
2025-12-17 14:23:25 +0530

    feat: update dashboard and doctor controller

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -3,10 +3,13 @@ import { useContext } from 'react'
 import { AdminContext } from '../../context/AdminContext'
 import { useEffect } from 'react'
 import { assets } from '../../assets/assets'
+import { AppContext } from '../../context/AppContext'
 
 const Dashboard = () => {
 
   const { aToken, getDashData, cancelAppointment, dashData } = useContext(AdminContext)
+
+  const {slotDateFormat} = useContext(AppContext)
 
   useEffect(() => {
     if (aToken) {
@@ -54,11 +57,11 @@ const Dashboard = () => {
         <div className='pt-4 border border-t-0'>
           {
             dashData.latestAppointments.map((item, index) => (
-              <div key={index}>
-                <img src={item.docData.image} alt="" />
-                <div>
-                  <p>{item.docData.name}</p>
-                  <p>{item.slotDate}</p>
+              <div className='flex items-center px-6 py-3 gap-3 hover:bg-gray-100' key={index}>
+                <img className='rounded-full w-10' src={item.docData.image} alt="" />
+                <div className='flex-1 text-sm'>
+                  <p className='text-gray-800 font-medium'>{item.docData.name}</p>
+                  <p className='text-gray-600'>{slotDateFormat(item.slotDate)}</p>
                 </div>
                 {item.cancelled
                   ? <p className='text-red-400 text-xs font-medium' >Cancelled</p>
--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,4 +1,6 @@
 import doctorModel from "../models/doctorModel.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const changeAvailability = async (req,res) => {
     try {
@@ -16,6 +18,30 @@ const doctorList = async (req,res) => {
     try {
         const doctors = await doctorModel.find({}).select(['-password','-email'])
         res.json({success:true,doctors})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+//doctor login
+const loginDoctor = async (req,res) => {
+    try {
+
+        const { email, password } = req.body
+        const doctor = await doctorModel.findOne({})
+        
+        if(!doctor) {
+            return res.json({success:false,message:'Invalid credentials'})
+        }
+
+        const isMatch = await bcrypt.compare(password, doctor.password)
+
+        if (isMatch) {
+            const token = jwt.sign({id:doctor-_id},process.env.JWT_SECRET)
+            res.json({success:true,token})
+        }
+
     } catch (error) {
         console.log(error)
         res.json({success:false,message:error.message})
